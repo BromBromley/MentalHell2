8c76dbf0af60ba905c853ea2f0a78071862bc9a2
Zeltoss <92942461+Zeltoss@users.noreply.github.com>
2025-06-09 00:59:18 +0200

    Dev UI Update
    - Added Reload Button to Level1
    - Added Main Menu Buttonn to Level1
    - Added WIP Text to Level1
    - Small touch ups

--- Assets/Scripts/UI Scripts/LevelButtonManager.cs
+++ Assets/Scripts/UI Scripts/LevelButtonManager.cs
@@ -10,5 +10,23 @@ public class LevelButtonManager : MonoBehaviour
     void Start()
     {
         root = uiDocument.rootVisualElement;
+
+        var mainMenuBtn = root.Q<VisualElement>("MainMenuBtn");
+        var reloadBtn = root.Q<VisualElement>("ReloadBtn");
+
+        mainMenuBtn.RegisterCallback<ClickEvent>(MainMenuBtnFunction);
+        reloadBtn.RegisterCallback<ClickEvent>(ReloadBtnFunction);
+    }
+
+    private void MainMenuBtnFunction(ClickEvent evt)
+    {
+        Debug.Log("Switched Back to Main Menu");
+        SceneManager.LoadScene (sceneBuildIndex:0);
+    }
+
+    private void ReloadBtnFunction(ClickEvent evt)
+    {
+        Debug.Log("Reloaded Level");
+        SceneManager.LoadScene (sceneBuildIndex:1);
     }
 }
