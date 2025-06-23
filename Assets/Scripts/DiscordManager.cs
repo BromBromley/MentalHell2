8f7e1dd97aed65f019be08c7e78e8ee29e53d50b
Zeltoss <92942461+Zeltoss@users.noreply.github.com>
2025-06-23 16:21:19 +0200

    Discord Status Update
    - Removed Discord Script from LevelUI Gameobject
    - Added "DontDestroyOnLoad" & "Singleton" Functionality to Discord Script
    - Added "DiscordStatus" Gamobject
    - Small touch ups to Discord Status Text

--- Assets/Scripts/DiscordManager.cs
+++ Assets/Scripts/DiscordManager.cs
@@ -4,12 +4,32 @@ using System.Collections.Generic;
 
 public class DiscordManager : MonoBehaviour
 {
-    Discord.Discord discord;
+    public static DiscordManager Instance;
+
+    private Discord.Discord discord;
+
+    void Awake()
+    {
+        // Singleton-Schutz
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 
     void Start()
     {
         discord = new Discord.Discord(1385606125797376030, (ulong)Discord.CreateFlags.Default);
         ChangeActivity();
+    }
+
+    void Update()
+    {
+        discord.RunCallbacks();
     }
 
     void OnDisable()
@@ -20,21 +40,22 @@ public class DiscordManager : MonoBehaviour
     public void ChangeActivity()
     {
         var activityManager = discord.GetActivityManager();
-        var activity = new Discord.Activity {
+        var activity = new Discord.Activity
+        {
             State = "Playing",
-            Details = "Currently Developing"
+            Details = "Tech Test Demo"
         };
-        activityManager.UpdateActivity(activity, (res) => {
-            if (res == Discord.Result.Ok) {
+
+        activityManager.UpdateActivity(activity, (res) =>
+        {
+            if (res == Discord.Result.Ok)
+            {
                 Debug.Log("Activity updated!");
-            } else {
+            }
+            else
+            {
                 Debug.LogError("Failed to update activity: " + res);
             }
         });
-    }
-
-    void Update()
-    {
-        discord.RunCallbacks();
     }
 }
